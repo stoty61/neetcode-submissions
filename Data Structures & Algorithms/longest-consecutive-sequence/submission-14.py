1d150880f1dede5d4d35a2ca2935785b4ecd35b9
Cole Stotland <83924153+stoty61@users.noreply.github.com>
2026-06-19 14:22:06 -0400

    Add: longest-consecutive-sequence - submission-14

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-14.py
@@ -0,0 +1,37 @@
+import heapq
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0 or len(nums) == 1:
+            return len(nums)
+
+        heapq.heapify(nums) 
+
+        longest = 1
+        temp = 1
+        value = nums[0]
+
+        for i in range(len(nums)):
+            smallest = heapq.heappop(nums)
+            if smallest == value:
+                continue
+            
+            if smallest == value + 1:
+                temp += 1
+
+            elif smallest > value + 1:
+                temp = 1
+
+
+            if temp > longest:
+                longest = temp
+               
+            value = smallest
+
+            print(smallest)
+            print("temp ", temp)
+            print("longest ", longest)
+
+
+        return longest
+        

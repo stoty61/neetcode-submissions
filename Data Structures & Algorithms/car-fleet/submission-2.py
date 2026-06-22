f861ea6c03c9584f11fe5891cd4c56b7cdfd3f04
Cole Stotland <83924153+stoty61@users.noreply.github.com>
2026-06-22 13:09:31 -0400

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,41 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        dic = {}
+        n = len(position)
+        for i, p in enumerate(position):
+            dic[p] = speed[i]
+
+        position.sort()
+        new_speeds = [0] * n
+
+        for i, p in enumerate(position):
+            new_speeds[i] = dic[p]
+
+     
+        remaining = [(target - position[i])/new_speeds[i] for i in range(n)]
+        print(dic)
+        print(position)
+        print(new_speeds)
+        print(remaining)
+
+        stack = []
+        tot = 1
+        for i in range(n-1,-1,-1):
+            if not stack:
+                stack.append(remaining[i])
+                continue
+            
+            if remaining[i] > stack[-1]:
+                tot += 1
+                stack.append(remaining[i])
+
+
+
+
+
+
+        return tot
+        
+
+
+        
